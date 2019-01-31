3404f3152e7c792331e03507ead0c54a1274ea04
Daniel Ferreira <djf989@gmail.com>
2019-01-31 16:05:10 -0200

    fix: xmonad config

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,68 +1,97 @@
 import           XMonad
-import           XMonad.Hooks.DynamicLog
-import           XMonad.Hooks.ManageDocks
-import           XMonad.Hooks.ManageHelpers
-import           System.Exit
+import           XMonad.Hooks.DynamicLog        ( dynamicLogWithPP
+                                                , PP(..)
+                                                , xmobarColor
+                                                , xmobarPP
+                                                )
+import           XMonad.Hooks.ManageDocks       ( avoidStruts
+                                                , docks
+                                                , manageDocks
+                                                , docksEventHook
+                                                )
+import           XMonad.Hooks.ManageHelpers     ( doCenterFloat
+                                                , isDialog
+                                                )
+import           System.Exit                    ( exitWith
+                                                , ExitCode(..)
+                                                )
+import           Graphics.X11.ExtraTypes.XF86   ( xF86XK_AudioRaiseVolume
+                                                , xF86XK_AudioLowerVolume
+                                                , xF86XK_AudioMute
+                                                , xF86XK_MonBrightnessUp
+                                                , xF86XK_MonBrightnessDown
+                                                )
+
+import           XMonad.Hooks.EwmhDesktops      ( fullscreenEventHook )
 import           XMonad.Config.Desktop
-import           Graphics.X11.ExtraTypes.XF86
-
-import           XMonad.Layout.Spacing
-import           XMonad.Hooks.EwmhDesktops
-
 import qualified XMonad.StackSet               as W
 import qualified Data.Map                      as M
-import           Data.Monoid
+import           Data.Monoid                    ( All(..) )
+import           XMonad.Util.SpawnOnce          ( spawnOnce )
+import           XMonad.Layout.NoBorders        ( smartBorders )
+import           XMonad.Util.Run                ( spawnPipe )
 
-main = xmonad =<< statusBar "xmobar" myPP toggleStrutsKey myConfig
+import           System.IO                      ( hPutStrLn )
 
-myConfig = desktopConfig { modMask            = mod4Mask
-                         , borderWidth        = myBorderWidth
-                         , normalBorderColor  = myNormalBorderColor
-                         , focusedBorderColor = myFocusedBorderColor
-                         , terminal           = "termite"
-                         , manageHook         = myManageHook
-                         , layoutHook         = myLayoutHook
-                         , handleEventHook    = myEventHook
-                         , workspaces         = myWorkspaces
-                         , keys               = myKeys
-                         }
+main = do
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ desktopConfig
+    { modMask            = mod4Mask
+    , borderWidth        = myBorderWidth
+    , normalBorderColor  = myNormalBorderColor
+    , focusedBorderColor = myFocusedBorderColor
+    , terminal           = myTerminal
+    , workspaces         = myWorkspaces
+    , keys               = myKeys
+    , manageHook         = myManageHook <+> manageHook desktopConfig
+    , layoutHook         = myLayoutHook $ layoutHook desktopConfig
+    , logHook            = myLogHook xmproc <+> logHook desktopConfig
+    , handleEventHook    = myEventHook <+> handleEventHook desktopConfig
+    , startupHook        = myStartupHook <+> startupHook desktopConfig
+    }
 
-
--- Visual
+myTerminal = "termite"
 myBorderWidth = 3
 myNormalBorderColor = "#c5c8c6"
 myFocusedBorderColor = "#F7486F"
-
 myWorkspaces = ["\xf268", "\xf121", "\xf120", "\xf008", "\xf07b", "\xf3ed"]
 
-myLayoutHook = spacing 10 $ avoidStruts $ layoutHook desktopConfig
+myLayoutHook = smartBorders
 
 myManageHook = composeAll
   [ className =? "Google-chrome" --> doShift (myWorkspaces !! 0)
-  , className =? "code" --> doShift (myWorkspaces !! 1)
-  , className =? "mpv" --> doShift (myWorkspaces !! 3) <+> doFloat
+  , className =? "code-oss" --> doShift (myWorkspaces !! 1)
+  , className =? "mpv" --> doShift (myWorkspaces !! 3) <+> doCenterFloat
+  , className =? "Pcmanfm" --> doShift (myWorkspaces !! 4)
   , className =? "openfortiGUI" --> doShift (myWorkspaces !! 5)
   , className =? "Peek" --> doFloat
   , isDialog --> doCenterFloat
-  , manageDocks
-  , manageHook def
   ]
 
-myEventHook = fullscreenEventHook <+> removeBordersEventHook
+myEventHook = fullscreenEventHook
+
+myLogHook proc = do
+  dynamicLogWithPP xmobarPP { ppOutput          = hPutStrLn proc
+                            , ppOrder           = \(ws : l : t : _) -> [ws]
+                            , ppCurrent         = xmobarColor "#F7296D" ""
+                            , ppHidden          = xmobarColor "#c5c8c6" ""
+                            , ppHiddenNoWindows = xmobarColor "#373b41" ""
+                            , ppUrgent          = xmobarColor "#198844" ""
+                            , ppLayout          = xmobarColor "#c5c8c6" ""
+                            }
+
+myStartupHook = spawnOnce "compton -b"
 
 -- Keyboard shortcuts
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig { XMonad.modMask = modMask }) =
   M.fromList
-    $  [ (-- launch a terminal
-           (modMask, xK_Return)
-         , spawn $ XMonad.terminal conf
-         )
-       , ( (modMask, xK_d)
-         , spawn " ~/.config/rofi/scripts/drun"
-         )
-         -- screenshot
-       , ((0, xK_Print), spawn "maim | xclip -select c -t image/png")
+    $  [ -- launch a terminal
+         ((modMask, xK_Return), spawn $ XMonad.terminal conf)
+       , -- launch ROFI 
+         ((modMask, xK_d)     , spawn " ~/.config/rofi/scripts/drun")
+       , -- screenshot
+         ((0, xK_Print)       , spawn "maim | xclip -select c -t image/png")
        , ( (modMask, xK_Print)
          , spawn
            "maim -i $(xdotool getactivewindow) | xclip -select c -t image/png"
@@ -70,34 +99,26 @@ myKeys conf@(XConfig { XMonad.modMask = modMask }) =
        , ( (modMask .|. shiftMask, xK_Print)
          , spawn "maim -s | xclip -select c -t image/png"
          )
-    -- multimedia
-       , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -D pulse sset Master 2%+")
+       ,
+         -- multimedia
+         ((0, xF86XK_AudioRaiseVolume), spawn "amixer -D pulse sset Master 2%+")
        , ((0, xF86XK_AudioLowerVolume), spawn "amixer -D pulse sset Master 2%-")
        , ((0, xF86XK_AudioMute), spawn "amixer -D pulse sset Master toggle")
-       , ((0, xF86XK_MonBrightnessUp) , spawn "xbacklight -inc 10")
-       , ( (0, xF86XK_MonBrightnessDown)
-         , spawn "xbacklight -dec 10"
-         )
-     -- Swap the focused window and the master window
-       , ( (modMask .|. shiftMask, xK_Return)
-         , windows W.swapMaster
-         )
-     -- close focused window
-       , ( (modMask, xK_q)
-         , kill
-         )
-         --  Reset the layouts on the current workspace to default
-       , ( (modMask .|. shiftMask, xK_space)
-         , setLayout $ XMonad.layoutHook conf
-         )
-
-    -- Push window back into tiling
-       , ( (modMask, xK_t)
-         , withFocused $ windows . W.sink
-         )
-
-     -- Restart xmonad
-       , ( (modMask .|. shiftMask, xK_r)
+       , ((0, xF86XK_MonBrightnessUp)       , spawn "xbacklight -inc 10")
+       , ((0, xF86XK_MonBrightnessDown)     , spawn "xbacklight -dec 10")
+       , -- Swap the focused window and the master window
+         ((modMask .|. shiftMask, xK_Return), windows W.swapMaster)
+       , -- close focused window
+         ((modMask, xK_q)                   , kill)
+       ,
+         -- Reset the layouts on the current workspace to default
+         ((modMask .|. shiftMask, xK_space) , setLayout $ layoutHook conf)
+       , -- Rotate through the available layout algorithms
+         ((modMask, xK_space)               , sendMessage NextLayout)
+       , -- Push window back into tiling
+         ((modMask, xK_t)                   , withFocused $ windows . W.sink)
+       , -- Restart xmonad
+         ( (modMask .|. shiftMask, xK_r)
          , spawn "xmonad --recompile; xmonad --restart"
          )
 
@@ -108,20 +129,9 @@ myKeys conf@(XConfig { XMonad.modMask = modMask }) =
     -- mod-[1..9] %! Switch to workspace N
     -- mod-shift-[1..9] %! Move client to workspace N
        [ ((m .|. modMask, k), windows $ f i)
-       | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+       | (i, k) <- zip (workspaces conf) [xK_1 .. xK_9]
        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
        ]
-
-
-myPP = xmobarPP { ppOrder           = \(ws : l : t : _) -> [ws]
-                , ppCurrent         = xmobarColor "#F7296D" ""
-                , ppHidden          = xmobarColor "#c5c8c6" ""
-                , ppHiddenNoWindows = xmobarColor "#373b41" ""
-                , ppUrgent          = xmobarColor "#198844" ""
-                , ppLayout          = xmobarColor "#c5c8c6" ""
-                }
-
-toggleStrutsKey XConfig { XMonad.modMask = modMask } = (modMask, xK_b)
 
 removeBordersEventHook :: Event -> X All
 removeBordersEventHook ev = do
