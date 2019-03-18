7965953c757b9d6dfacfffcba43283176ffc6344
Daniel Ferreira <djf989@gmail.com>
2019-03-18 02:15:44 -0300

    update install-zsh

--- scripts/install-zsh.sh
+++ scripts/install-zsh.sh
@@ -4,11 +4,11 @@
 sudo pacman -S zsh zsh-completions
 
 #Change default shell for user
-chsh -s /bin/sh
+chsh -s /bin/zsh
 
 #Install Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #Install Spaceship Prompt theme
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "~/.oh-my-zsh/custom/themes/spaceship-prompt"
+ln -s "~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "~/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
